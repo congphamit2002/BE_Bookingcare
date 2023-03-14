bbe27fbf331203cbf1f0936ea86ab062ea1ffc5c
cong pham <congphamit22072002@gmail.com>
2023-03-14 21:58:05 +0700

    create models and migrations

--- src/seeders/seeders-user.js
+++ src/seeders/seeders-user.js
@@ -3,10 +3,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+    return queryInterface.bulkInsert('users', [{
+      email: 'thanhcongpham22072002@gmail.com',
+      password: 'congpro123',
+      firstName: 'Cong',
+      lastName: 'Pham',
+      address: 'Quang Nam',
+      gender: 1,
+      roleId: 'R1',
+      phoneNumber: '0382412729',
+      positionId: 'P1',
+      image: 'cong.jpg',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
